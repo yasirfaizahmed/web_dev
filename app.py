734ba7606f15c077d88125c0142491f73c9c3920
yasirfaizahmed <yasirfaizahmed.n@gmail.com>
2023-11-14 00:39:26 +0530

    working code with UI

--- app.py
+++ app.py
@@ -1,37 +1,53 @@
-from flask import Flask, request
+import os
+from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 
+from sqlalchemy.sql import func
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-with app.app_context():
-  db.create_all()
 
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(100), nullable=False)
+    lastname = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+    age = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime(timezone=True),
+                           server_default=func.now())
+    bio = db.Column(db.Text)
 
-class Drink(db.Model):
-  def __init__(self, name):
-    self.name = name
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(50))
+    def __repr__(self):
+        return f'<Student {self.firstname}>'
 
 
 @app.route("/")
 def index():
-  return "hello"
+    students = Student.query.all()
+    return render_template('index.html', students=students)
+    # return "Home"
 
 
-@app.route("/drinks", methods=["POST"])
-def drinks():
-  drink = Drink(name=request.json['name'])
-  with app.app_context():
-    db.session.add(drink)
-    db.session.commit()
-    return {"drink": drink.name}
+@app.route("/students/", methods=["GET"])
+def students():
+    output = {}
+    students = Student.query.all()
+    for student in students:
+        output.update({"firstname": student.firstname,
+                       "lastname": student.lastname,
+                       "email": student.email,
+                       "age": student.age,
+                       "created_at": student.created_at,
+                       "bio": student.bio
+                       })
+    return output
 
 
-if __name__ == '__main__':
-  app.run()
+
